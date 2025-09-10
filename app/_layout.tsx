50e82c7e7f6bc9196da824736711165c8655200b
Yair Kosowsky-Sachs <y.k.sachs@gmail.com>
2025-09-09 22:30:51 -0400

    Use guest ids in db

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,13 @@
 import { Stack } from "expo-router";
 import { SQLiteDatabase, SQLiteProvider } from "expo-sqlite";
 import React from "react";
-import { AutocompleteDropdownContextProvider } from "react-native-autocomplete-dropdown";
 
 export default function RootLayout() {
     async function initDB(db: SQLiteDatabase) {
         db.execSync(`
             PRAGMA journal_mode = WAL;
             CREATE TABLE IF NOT EXISTS meals (id TEXT NOT NULL PRIMARY KEY, date TEXT NOT NULL);
-            CREATE TABLE IF NOT EXISTS guests (meal_id TEXT NOT NULL, name TEXT NOT NULL, status TEXT NOT NULL, FOREIGN KEY(meal_id) REFERENCES meals(id), PRIMARY KEY(name, meal_id));
+            CREATE TABLE IF NOT EXISTS guests (meal_id TEXT NOT NULL, guest_id TEXT NOT NULL, name TEXT NOT NULL, status TEXT NOT NULL, FOREIGN KEY(meal_id) REFERENCES meals(id), PRIMARY KEY(guest_id, meal_id));
         `);
     }
 
